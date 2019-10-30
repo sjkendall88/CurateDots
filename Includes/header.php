b208978e33172d67b60e76980ae8d17be011af09
sjkendall88 <sjkendall88@byui.edu>
2019-10-29 23:46:36 -0500

    add some images and CSS

--- Includes/header.php
+++ Includes/header.php
@@ -1,25 +1,33 @@
 <header>
-    <img src="" alt="Curated Dots Logo">
-    <h1>Curated Dots</h1>
+    <!-- anchor image as a hyper-link to home-page. -->
+<a href="index.php"><img src="Images/home.jpg" alt="homeButton"></a>
+<img src="Images/notification.jpg" alt="notification"/>
+<!-- Home page linked through logo -->
+<a href="index.php"><img src="Images/CD-Logo.jpg" alt="Curated Dots Logo" id="logo"></a>
+    <h1 id="siteTitle">Curated Dots</h1>
     <form name="Login" action="index.php" method="post">
-        <label>
-            Username
-            <input type="text" name="username" placeholder="username">
-        </label><br>root
-        <label>
-            Password
-            <input type="password" name="password">
-        </label><br>
         <?php
             if($_SERVER["REQUEST_METHOD"] == "POST"){
                 if (!$logonSuccess){
-                    echo "Invalid name and/or password";
+                    echo "Invalid name and/or password<br>";
                 }
             }
         ?>
-        <br>
+        <label>
+            Username:
+            <input type="text" name="username" placeholder="username">
+        </label><br>
+        <label>
+            Password:
+            <input type="password" name="password">
+        </label><br>
         <input type="submit" value="Login">
+        <input type="submit" formaction="createNewAccount.php" value="Create Curator">
         <a href="" name="forgot password">Forgot Password</a>
     </form>
+    <form name="showDots" method="GET" action="login.php">
+        Show Dots: <input type="text" name="user" placeholder="username" /><br>
+        <input type="submit" value="Show" />
+        
+    </form>
 </header>
-
--- index.php
+++ index.php
@@ -19,23 +19,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>CurateDots</title>
+            <link rel="stylesheet" href="CSS/cssSmall.css"/>
+            <link rel="stylesheet" href="CSS/cssMed.css"/>
+            <link rel="stylesheet" href="CSS/css"/>
             <link href="CSS/normalization.css" rel="stylesheet">
         </head>
 
         <body>
-        <h4>Test title before php</h4>
             <?php
                 $cwd = getcwd();
                 include($cwd.'/includes/header.php');
             ?>
-            <h4>Test title after php</h4>
-            
-            <form name="login" method="GET" action="login.php">
-                Show Dots: <input type="text" name="user" value="" />
-                <input type="submit" value="Go" />
-            </form>
-            <br>Still Don't have a wish list?! <a href="createNewAccount.php">Create Now</a>
-            
         </body>
     </html>
 </DOCTYPE>
